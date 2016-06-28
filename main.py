603560dfac72495b6f063002faa2586802e1210f
Utkarsh Gupta <utkarshg1408@gmail.com>
2016-06-28 15:35:48 +0530

    Extract Bing Photo of the Day and save it in the root directory

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ from bs4 import BeautifulSoup
 BingXML_URL = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US"
 page = urllib2.urlopen(BingXML_URL)
 BingXML = BeautifulSoup(page, "lxml")
+
+Images = BingXML.find_all('image')
+ImageURL = "https://www.bing.com" + Images[0].url.text
+urllib.urlretrieve(ImageURL, Images[0].startdate.text+".jpg")
