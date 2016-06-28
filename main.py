1d58b6640e6f2093f34030205e2b50a08eacfece
Utkarsh Gupta <utkarshg1408@gmail.com>
2016-06-28 17:28:05 +0530

    Successfully implemented wallpaper changing feature

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import urllib2
 import urllib
 from bs4 import BeautifulSoup
+import getpass
 
 BingXML_URL = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US"
 page = urllib2.urlopen(BingXML_URL)
@@ -12,4 +13,17 @@ BingXML = BeautifulSoup(page, "lxml")
 
 Images = BingXML.find_all('image')
 ImageURL = "https://www.bing.com" + Images[0].url.text
-urllib.urlretrieve(ImageURL, Images[0].startdate.text+".jpg")
+ImageName = Images[0].startdate.text+".jpg"
+
+username = getpass.getuser()
+path = '/home/' + username + '/Pictures/BingWallpapers/'
+if not os.path.exists(path):
+	os.makedirs(path)
+	print "New Directory Created"
+os.chdir(path)
+if not os.path.isfile(ImageName):
+	urllib.urlretrieve(ImageURL, ImageName)
+	os.system('notify-send "'+'Bing Wallpaper updated successfully'+'" "'+ Images[0].copyright.text.encode('utf-8') +'"')
+	os.system("gsettings set org.gnome.desktop.background picture-uri file:" + path + ImageName )
+else:
+	os._exit(1)
