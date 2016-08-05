9b1a6ab07958c25a5fa8a819d300c4d3e16baf65
Lucas Meneghel Rodrigues <lookkas@gmail.com>
2016-08-04 22:41:16 -0300

    main.py: Fix comment typo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 
 # Get BingXML file which contains the URL of the Bing Photo of the day
 # idx = Number days previous the present day. 0 means current day, 1 means yesterday, etc
-# n = Number of images predious the day given by idx
+# n = Number of images previous the day given by idx
 # mkt denotes your location. e.g. en-US means United States. Put in your
 # country code
 BingXML_URL = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US"
