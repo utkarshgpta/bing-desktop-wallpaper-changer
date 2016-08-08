utkarshgpta/bing-desktop-wallpaper-changer

commit ec2c2669c1973abfcd20076e871ddd22c95fe2ad

port bs4 to element tree, remove dependency

main.py
@@ -8,7 +8,8 @@ import sys
 import urllib
 import urllib2
 
-from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
+#from bs4 import BeautifulSoup
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -37,9 +38,9 @@ def get_valid_bing_markets():
     """
     url = 'https://msdn.microsoft.com/en-us/library/dd251064.aspx'
     page = urllib2.urlopen(url)
-    page_xml = BeautifulSoup(page, 'lxml')
+    page_xml = ET.parse(page).getroot()
     # Look in the table data
-    market = page_xml.find_all('td')
+    market = page_xml.findall('td')
     market_locales = [el[1].text.strip() for el in enumerate(market) if
                       el[0] % 2 == 0]
     return market_locales
@@ -82,9 +83,12 @@ def get_image_metadata():
     """
     bing_xml_url = get_bing_xml()
     page = urllib2.urlopen(bing_xml_url)
-    bing_xml = BeautifulSoup(page, "lxml")
+
+    bing_xml = ET.parse(page).getroot()
+
     # For extracting complete URL of the image
-    images = bing_xml.find_all('image')
+    images = bing_xml.findall('image')
+    print(images[0].find("startdate").text)
     return images[0]
 
 
@@ -95,7 +99,7 @@ def get_image_url(metadata):
     :param metadata: XML tag object with image metadata.
     :return: URL with Bing Wallpaper image.
     """
-    base_image = metadata.url.text
+    base_image = metadata.find("url").text
     # Replace image resolution with the correct resolution
     # from your main monitor
     screen_size = get_screen_resolution_str()
@@ -121,8 +125,10 @@ def main():
     Notify.init(app_name)
     exit_status = 0
     try:
+
         image_metadata = get_image_metadata()
-        image_name = image_metadata.startdate.text + ".jpg"
+
+        image_name = image_metadata.find("startdate").text + ".jpg"
         image_url = get_image_url(image_metadata)
         # Images saved to '/home/[user]/Pictures/BingWallpapers/'
         download_path = os.path.join(os.path.expanduser('~'), 'Pictures',
@@ -135,17 +141,17 @@ def main():
             bg_changer = BackgroundChanger()
             bg_changer.change_background(image_path)
             summary = 'Bing Wallpaper updated successfully'
-            body = image_metadata.copyright.text.encode('utf-8')
+            body = image_metadata.find("copyright").text.encode('utf-8')
         else:
             summary = 'Bing Wallpaper unchanged'
             body = ('%s already exists in Wallpaper directory' %
-                    image_metadata.copyright.text.encode('utf-8'))
+                    image_metadata.find("copyright").text.encode('utf-8'))
     except Exception as err:
         summary = 'Error executing %s' % app_name
         body = err
         exit_status = 1
 
-    app_notification = Notify.Notification.new(summary, body)
+    app_notification = Notify.Notification.new(summary, str(body))
     app_notification.show()
     sys.exit(exit_status)
 

commit 044ecafe0b0f7df980070213c807a1eceacf58d8

main.py: Hardcode valid bing market areas
I noticed that ec2c2669c1973abfcd20076e871ddd22c95fe2ad broke
the functionality to look up the MSDN page to find valid
Bing markets. Since the market information isn't expected
to change frequently, let's simply hardcode it in the script
instead. As a nice side effect, this will speed up the script
execution a bit, besides fixing the bug introduced with the
commit above.

Signed-off-by: Lucas Meneghel Rodrigues <lookkas@gmail.com>

main.py
@@ -23,6 +23,65 @@ from gi.repository import Gio
 from gi.repository import Gtk
 from gi.repository import Notify
 
+BING_MARKETS = [u'ar-XA',
+                u'bg-BG',
+                u'cs-CZ',
+                u'da-DK',
+                u'de-AT',
+                u'de-CH',
+                u'de-DE',
+                u'el-GR',
+                u'en-AU',
+                u'en-CA',
+                u'en-GB',
+                u'en-ID',
+                u'en-IE',
+                u'en-IN',
+                u'en-MY',
+                u'en-NZ',
+                u'en-PH',
+                u'en-SG',
+                u'en-US',
+                u'en-XA',
+                u'en-ZA',
+                u'es-AR',
+                u'es-CL',
+                u'es-ES',
+                u'es-MX',
+                u'es-US',
+                u'es-XL',
+                u'et-EE',
+                u'fi-FI',
+                u'fr-BE',
+                u'fr-CA',
+                u'fr-CH',
+                u'fr-FR',
+                u'he-IL',
+                u'hr-HR',
+                u'hu-HU',
+                u'it-IT',
+                u'ja-JP',
+                u'ko-KR',
+                u'lt-LT',
+                u'lv-LV',
+                u'nb-NO',
+                u'nl-BE',
+                u'nl-NL',
+                u'pl-PL',
+                u'pt-BR',
+                u'pt-PT',
+                u'ro-RO',
+                u'ru-RU',
+                u'sk-SK',
+                u'sl-SL',
+                u'sv-SE',
+                u'th-TH',
+                u'tr-TR',
+                u'uk-UA',
+                u'zh-CN',
+                u'zh-HK',
+                u'zh-TW']
+
 
 class BackgroundChanger(object):
     SCHEMA = 'org.gnome.desktop.background'
@@ -45,23 +104,6 @@ config_file_skeleton = """[market]
 # https://msdn.microsoft.com/en-us/library/dd251064.aspx
 area =
 """
-
-
-def get_valid_bing_markets():
-    """
-    Find valid Bing markets for area auto detection.
-
-    :see: https://msdn.microsoft.com/en-us/library/dd251064.aspx
-    :return: List with valid Bing markets (list looks like a list of locales).
-    """
-    url = 'https://msdn.microsoft.com/en-us/library/dd251064.aspx'
-    page = urlopen(url)
-    page_xml = ET.parse(page).getroot()
-    # Look in the table data
-    market = page_xml.findall('td')
-    market_locales = [el[1].text.strip() for el in enumerate(market) if
-                      el[0] % 2 == 0]
-    return market_locales
 
 
 def get_config_file():
@@ -99,7 +141,7 @@ def get_market():
         return market_area_override
 
     default_locale = locale.getdefaultlocale()[0]
-    if default_locale in get_valid_bing_markets():
+    if default_locale in BING_MARKETS:
         return default_locale
 
     return 'en-US'